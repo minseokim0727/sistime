051c441059d2138aba74edb128a94867d158ffe2
kelio <kelio@DESKTOP-M4P6QB2>
2024-06-02 21:20:22 +0900

    푸시 - 김민석

---
+++ sistime/src/main/java/com/sist/domain/TimeDTO.java
@@ -0,0 +1,97 @@
+package com.sist.domain;
+
+public class TimeDTO {
+	private int sub_num;
+	private String sub_name;
+	private String sub_pro;
+	private String sub_content;
+	private String sub_year;
+	private String semester;
+	private int sub_grade;
+	private int univ_num;
+	private String sub_date;
+	private String sub_start;
+	private String sub_end;
+	private int tot_grade;
+	private String email;
+	
+	public int getSub_num() {
+		return sub_num;
+	}
+	public void setSub_num(int sub_num) {
+		this.sub_num = sub_num;
+	}
+	public String getSub_name() {
+		return sub_name;
+	}
+	public void setSub_name(String sub_name) {
+		this.sub_name = sub_name;
+	}
+	public String getSub_pro() {
+		return sub_pro;
+	}
+	public void setSub_pro(String sub_pro) {
+		this.sub_pro = sub_pro;
+	}
+	public String getSub_content() {
+		return sub_content;
+	}
+	public void setSub_content(String sub_content) {
+		this.sub_content = sub_content;
+	}
+	public String getSub_year() {
+		return sub_year;
+	}
+	public void setSub_year(String sub_year) {
+		this.sub_year = sub_year;
+	}
+	public String getSemester() {
+		return semester;
+	}
+	public void setSemester(String semester) {
+		this.semester = semester;
+	}
+	public int getSub_grade() {
+		return sub_grade;
+	}
+	public void setSub_grade(int sub_grade) {
+		this.sub_grade = sub_grade;
+	}
+	public int getUniv_num() {
+		return univ_num;
+	}
+	public void setUniv_num(int univ_num) {
+		this.univ_num = univ_num;
+	}
+	public String getSub_date() {
+		return sub_date;
+	}
+	public void setSub_date(String sub_date) {
+		this.sub_date = sub_date;
+	}
+	public String getSub_start() {
+		return sub_start;
+	}
+	public void setSub_start(String sub_start) {
+		this.sub_start = sub_start;
+	}
+	public String getSub_end() {
+		return sub_end;
+	}
+	public void setSub_end(String sub_end) {
+		this.sub_end = sub_end;
+	}
+	public int getTot_grade() {
+		return tot_grade;
+	}
+	public void setTot_grade(int tot_grade) {
+		this.tot_grade = tot_grade;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
